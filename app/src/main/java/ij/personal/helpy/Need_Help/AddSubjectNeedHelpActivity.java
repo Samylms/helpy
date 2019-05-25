0e9354d90e6de9877769a2635b58ba8db085109a
Yvan <yvankiller92@gmail.com>
2019-05-25 09:37:20 +0200

    Ajout du spinner pour choisir la matière

--- app/src/main/java/ij/personal/helpy/Need_Help/AddSubjectNeedHelpActivity.java
+++ app/src/main/java/ij/personal/helpy/Need_Help/AddSubjectNeedHelpActivity.java
@@ -4,20 +4,30 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.List;
 
 import ij.personal.helpy.R;
 
 public class AddSubjectNeedHelpActivity extends AppCompatActivity {
 
+    private EditText editSubjectName;
+    private Spinner spinnerGeneralSubject;
     private Button btnValidate;
     private TextView txtCancel;
+
+    private List<String> listGeneralSubject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_subject_need_help);
 
+        editSubjectName = findViewById(R.id.edit_subject_name_need_help);
+        spinnerGeneralSubject = findViewById(R.id.spinner_general_subject_need_help);
         btnValidate = findViewById(R.id.btn_validate_add_subject_need_help);
         txtCancel = findViewById(R.id.btn_cancel_add_subject_need_help);
 
